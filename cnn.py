fb0ee210eb48c1d3f45c55498b565e4fbbcebae1
rlpddejong <78429975+rlpddejong@users.noreply.github.com>
2021-02-21 14:12:52 +0100

    Add files via upload

--- cnn.py
+++ cnn.py
@@ -2,6 +2,9 @@
 TU/e BME Project Imaging 2021
 Convolutional neural network for PCAM
 Author: Suzanne Wetstein
+Adjustments made by group 10 (02-2021):
+    1. Addition of ROC analysis function
+    2. Addition of FCN model
 '''
 
 # disable overly verbose tensorflow logging
@@ -14,7 +17,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPool2D
+from tensorflow.keras.layers import Conv1D, Conv2D, MaxPool2D, Dropout, BatchNormalization, GlobalMaxPooling2D
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 from tensorflow import keras
@@ -55,10 +58,8 @@ def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
 
 
 
-
 def get_model(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64):
-
-
+    
      # build the model
      model = Sequential()
 
@@ -79,13 +80,15 @@ def get_model(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filte
      return model
 
 
+
 # ROC analysis
 def ROC_curve(model, val_gen):
     
+    # obtain labels and predictions
     val_predict = model.predict_generator(val_gen)
     val_y = val_gen.classes
     
-    # Compute ROC curve and ROC area
+    # compute ROC curve and ROC area
     fpr = dict()
     tpr = dict()
     roc_auc = dict()
@@ -93,7 +96,7 @@ def ROC_curve(model, val_gen):
     fpr[0], tpr[0], _ = roc_curve(val_y, val_predict[:,0])
     roc_auc[0] = auc(fpr[0], tpr[0])
     
-    # Plot the ROC curve and show the area
+    # plot the ROC curve and show the area
     plt.figure()
     lw = 2
     plt.plot(fpr[0], tpr[0], color='darkorange',
@@ -108,3 +111,28 @@ def ROC_curve(model, val_gen):
     plt.show()
     
     return
+
+
+
+# Fully convolutional neural network (FCN) model 
+def get_model2(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64):
+     
+     # build the model
+     model = Sequential()
+     
+     # original convolutional layers
+     model.add(Conv2D(first_filters, kernel_size, activation = 'relu', padding = 'same', input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)))
+     model.add(MaxPool2D(pool_size = pool_size)) 
+
+     model.add(Conv2D(second_filters, kernel_size, activation = 'relu', padding = 'same'))
+     model.add(MaxPool2D(pool_size = pool_size))
+    
+     # added convolutional activation layers
+     model.add(Conv2D(64, (1,1), activation = 'relu', padding = 'same'))
+     model.add(Conv2D(1, (1,1), activation = 'sigmoid', padding = 'same'))   
+     model.add(GlobalMaxPooling2D())
+    
+     # compile the model
+     model.compile(SGD(lr=0.01, momentum=0.95), loss = 'binary_crossentropy', metrics=['accuracy'])
+    
+     return model
